74d03d48bbbf25a789e7154cce940d57ea2ea607
boburnhamjokes <spamprotection666@gmail.com>
2017-03-13 01:03:24 +0800

    Add files via upload

---
+++ bo.js
@@ -0,0 +1,371 @@
+var list = [{
+        setup: 'Bo goes on a cruise',
+        joke: 'Boat burnham'
+    },
+    {
+        setup: 'Bo likes satanic animals',
+        joke: 'Goat burnham'
+    },
+    {
+        setup: 'Bo becomes a deer',
+        joke: 'Doe burnham'
+    },
+    {
+        setup: 'Bo becomes a baker',
+        joke: 'Dough burnham'
+    },
+    {
+        setup: 'Bo becomes sp00py',
+        joke: 'Bone burnham'
+    },
+    {
+        setup: 'Bo is on the tip of his feet',
+        joke: 'Toe burnham'
+    },
+    {
+        setup: 'Bo becomes a farmer/seamster',
+        joke: 'Hoe/sow burnham'
+    },
+    {
+        setup: 'Bo becomes an archer',
+        joke: 'Bow burnham'
+    },
+    {
+        setup: 'Bo is good at making ribbons',
+        joke: 'Bow burnham'
+    },
+    {
+        setup: 'Bo doesn\'t like plates',
+        joke: 'Bowl burnham'
+    },
+    {
+        setup: 'Bo likes bragging about his achievements',
+        joke: 'Boast burnham'
+    },
+    {
+        setup: 'Bo gets hate comments',
+        joke: 'Roast burnham'
+    },
+    {
+        setup: 'Bo gets his own talkshow',
+        joke: 'Host burnham'
+    },
+    {
+        setup: 'Bo is a quick mailman',
+        joke: 'Post burnham'
+    },
+    {
+        setup: 'Bo is alike',
+        joke: 'Most burnham'
+    },
+    {
+        setup: 'Bo likes the beach',
+        joke: 'Coast burnham'
+    },
+    {
+        setup: 'Bo likes celebratory bread',
+        joke: 'Toast burnham'
+    },
+    {
+        setup: 'Bo is a book author',
+        joke: 'Wrote burnham'
+    },
+    {
+        setup: 'Bo is still a mummy\'s boy',
+        joke: 'Dote burnham'
+    },
+    {
+        setup: 'Bo is feeling confident',
+        joke: 'Bold burnham'
+    },
+    {
+        setup: 'Bo is feeling chilly',
+        joke: 'Cold burnham'
+    },
+    {
+        setup: 'Bo makes memorable speeches',
+        joke: 'Quote burnham'
+    },
+    {
+        setup: 'Bo has few fucks',
+        joke: 'Groat burnham'
+    },
+    {
+        setup: 'Bo still uses facebook',
+        joke: 'Poke burnham'
+    },
+    {
+        setup: 'Bo makes people laugh',
+        joke: 'Jokes burnham'
+    },
+    {
+        setup: 'Bo is laughed at by people',
+        joke: 'Joke burnham'
+    },
+    {
+        setup: 'Bo isn\'t real',
+        joke: 'Hoax burnham'
+    },
+    {
+        setup: 'Bo hangs out with his family',
+        joke: 'Folk burnham'
+    },
+    {
+        setup: 'Bo is good at persuasion',
+        joke: 'Coax burnham'
+    },
+    {
+        setup: 'Bo doesn\'t egg whites',
+        joke: 'Yolk burnham'
+    },
+    {
+        setup: 'Bo mislabels a homeless guy',
+        joke: 'Troll burnham'
+    },
+    {
+        setup: 'Bo hangs out with one of his relative',
+        joke: 'Both burnham'
+    },
+    {
+        setup: 'Bo thinks about the future',
+        joke: 'Bode burnham'
+    },
+    {
+        setup: 'Bo goes into hiding',
+        joke: 'Stow burnham'
+    },
+    {
+        setup: 'Bo carries lots of books',
+        joke: 'Tote burnham'
+    },
+    {
+        setup: 'Bo becomes a sushi chef',
+        joke: 'Roe burnham'
+    },
+    {
+        setup: 'Bo is used as manpower for boats',
+        joke: 'Row burnham'
+    },
+    {
+        setup: 'Bo makes lots of enemies',
+        joke: 'Foe burnham'
+    },
+    {
+        setup: 'Bo surprises people',
+        joke: 'Woah burnham'
+    },
+    {
+        setup: 'Bo becomes a female prostitute',
+        joke: 'Hoe burnham'
+    },
+    {
+        setup: 'Bo likes hip greetings',
+        joke: 'Yo burnham'
+    },
+    {
+        setup: 'Bo is cheered on',
+        joke: 'Go burnham'
+    },
+    {
+        setup: 'Bo is an average guy',
+        joke: 'Joe burnham'
+    },
+    {
+        setup: 'Bo doesn\'t like the name joe',
+        joke: 'Joel burnham'
+    },
+    {
+        setup: 'Bo wants to be a girl',
+        joke: 'Zoe burnham'
+    },
+    {
+        setup: 'Bo is naked',
+        joke: 'Clothe burnham'
+    },
+    {
+        setup: 'Bo has got sick raps',
+        joke: 'Flow burnham'
+    },
+    {
+        setup: 'Bo is shining',
+        joke: 'Glow burnham'
+    },
+    {
+        setup: 'Bo is feeling sad',
+        joke: 'Low burnham'
+    },
+    {
+        setup: 'Bo is playing an intense limbo game',
+        joke: 'Lower burnham'
+    },
+    {
+        setup: 'Bo replaces Jack Black in Kung Fu Panda',
+        joke: 'Po Burnham'
+    },
+    {
+        setup: 'Bo feels sorrow and distress',
+        joke: 'Woe burnham'
+    },
+    {
+        setup: 'Bo sucks off some guys',
+        joke: 'Blower burnham'
+    },
+    {
+        setup: 'Bo buys a new car',
+        joke: 'Rover burnham'
+    },
+    {
+        setup: 'Bo is in a good mood',
+        joke: 'Jovial burnham'
+    },
+    {
+        setup: 'Bo smokes weed',
+        joke: 'Stoner burnham'
+    },
+    {
+        setup: 'Bo goes to Delaware',
+        joke: 'Dover burnham'
+    },
+    {
+        setup: 'Bo\'s got his friends\' backs',
+        joke: 'Bro burnham'
+    },
+    {
+        setup: 'Bo struggles to keep up',
+        joke: 'Slow burnham'
+    },
+    {
+        setup: 'Bo is a genius',
+        joke: 'Pro burnham'
+    },
+    {
+        setup: 'Bo likes birds',
+        joke: 'Crow burnham'
+    },
+    {
+        setup: 'Bo has a bad crowd surfing experiencing',
+        joke: 'Throw burnham'
+    },
+    {
+        setup: 'Bo is getting even taller',
+        joke: 'Grow burnham'
+    },
+    {
+        setup: 'Bo takes care of his lawn',
+        joke: 'Mow burnham'
+    },
+    {
+        setup: 'Bo doesn\'t exist',
+        joke: 'No burnham'
+    },
+    {
+        setup: 'Bo is a stage character',
+        joke: 'Show burnham'
+    },
+    {
+        setup: 'Bo is in The Simpsons',
+        joke: 'Doh burnham'
+    },
+    {
+        setup: 'Bo helps to fix people\'s cars',
+        joke: 'Tow burnham'
+    },
+    {
+        setup: 'Bo is buff and in with current lingo',
+        joke: 'Swole burnham'
+    },
+    {
+        setup: 'Bo got shot in the chest with a rifle',
+        joke: 'Hole burnham'
+    },
+    {
+        setup: 'Bo feels complete',
+        joke: 'Whole burnham'
+    },
+    {
+        setup: 'Bo is a robber',
+        joke: 'Stole burnham'
+    },
+    {
+        setup: 'Bo makes smoke joints',
+        joke: 'Roll burnham'
+    },
+    {
+        setup: 'Bo is into analytics',
+        joke: 'Poll burnham'
+    },
+    {
+        setup: 'Bo becomes a black spot',
+        joke: 'Mole burnham'
+    },
+    {
+        setup: 'Bo goes to and',
+        joke: 'Fro burnham'
+    },
+    {
+        setup: 'Bo has a twin',
+        joke: 'Clone burnham'
+    },
+    {
+        setup: 'Bo has mobile number',
+        joke: 'Phone burnham'
+    },
+    {
+        setup: 'Bo has a statue of himself',
+        joke: 'Stone burnham'
+    },
+    {
+        setup: 'Bo does solo work',
+        joke: 'Lone burnham'
+    },
+    {
+        setup: 'Bo is sealing a business deal',
+        joke: 'Sold burnham'
+    },
+    {
+        setup: 'Bo talks in the third person',
+        joke: 'Told burnham'
+    },
+    {
+        setup: 'Bo is pinned down',
+        joke: 'Hold burnham'
+    },
+    {
+        setup: 'Bo\'s partner has',
+        joke: 'Rode burnham'
+    },
+    {
+        setup: 'Bo becomes an animal',
+        joke: 'Goat burnham'
+    },
+    {
+        setup: 'Bo goes to britain',
+        joke: 'Bloke burnham'
+    },
+    {
+        setup: 'Bo spends too much money ',
+        joke: 'Broke burnham'
+    },
+    {
+        setup: 'Bo is an activist',
+        joke: 'Woke burnham'
+    },
+    {
+        setup: 'Bo is judged by skeptical',
+        joke: 'So? burnham'
+    },
+    {
+        setup: 'When you want to make a pun off Bo\'s name but also don\'t want to because you\'ve made so many so you do ',
+        joke: 'Both burnham'
+    }
+]
+
+var box = document.getElementById('box');
+
+var i = Math.round(Math.random() * list.length, 0);
+box.innerHTML = list[i].setup + '<br><br>' + list[i].joke;
+
+document.body.addEventListener('click', function() {
+    i = Math.round(Math.random() * list.length, 0);
+    box.innerHTML = list[i].setup + '<br><br>' + list[i].joke;
+});
